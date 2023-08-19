f574411e1b66784de25bd3a8a14e3415a11a3fbf
shinta0806 <shinta.0806@gmail.com>
2023-08-19 10:16:31 +0900

    CsWin32 分離導入。

--- TestCsWin32/Views/MainPage.xaml.cs
+++ TestCsWin32/Views/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.UI.Xaml.Controls;
-
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using TestCsWin32.ViewModels;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 
 namespace TestCsWin32.Views;
 
@@ -15,5 +17,18 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private async void Button01_Click(Object _1, RoutedEventArgs _2)
+    {
+        HWND hwnd = PInvoke.GetForegroundWindow();
+        ContentDialog contentDialog = new()
+        {
+            Title = "情報",
+            Content = "ハンドル：" + hwnd.Value.ToString("X16"),
+            PrimaryButtonText = "OK",
+            XamlRoot = App.MainWindow.Content.XamlRoot,
+        };
+        await contentDialog.ShowAsync();
     }
 }
