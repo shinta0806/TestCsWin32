e7b0df2a2d98ee6ac4003e4bd9d085f53dc715e2
shinta0806 <shinta.0806@gmail.com>
2023-08-19 09:23:05 +0900

    プロジェクト ファイルを追加します。

---
+++ TestCsWin32/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TestCsWin32.ViewModels;
+
+public partial class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+}
---
+++ TestCsWin32/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using TestCsWin32.ViewModels;
+
+namespace TestCsWin32.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
